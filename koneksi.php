eb25548e5b846241669b56df119bd6c374ec4a0a
Mudri Katul Aliyah <mdrktul02aliya@gmail.com>
2025-09-22 16:25:32 +0700

    rev

--- koneksi.php
+++ koneksi.php
@@ -9,7 +9,7 @@ $user = "root";
 $pass = ""; // default Laragon root tanpa password
 $db   = "db_xirpl1-18_1";
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_connect("localhost", "root", "", "db_xirpl1-18_1");
 
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
