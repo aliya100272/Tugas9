3faa783865be5d1048250dcc04b4690194f9b952
Mudri Katul Aliyah <mdrktul02aliya@gmail.com>
2025-10-13 18:09:39 +0700

    pembenaran

--- koneksi.php
+++ koneksi.php
@@ -5,8 +5,8 @@ ini_set('display_errors', 1);
 
 <?php
 $host = "localhost";
-$user = "root";
-$pass = ""; 
+$user = "xirpl1-18";
+$pass = "3087045478"; 
 $db   = "db_xirpl1-18_1";
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
