42cffce97b52cafd0273deebc31517b9e16abccf
Mudri Katul Aliyah <mdrktul02aliya@gmail.com>
2025-10-18 21:13:15 +0700

    bnrin

--- koneksi.php
+++ koneksi.php
@@ -4,13 +4,16 @@ ini_set('display_errors', 1);
 ?>
 
 <?php
+// variable data koneksi
 $host = "localhost";
-$user = "root";
-$pass = ""; 
+$user = "xirpl1-18";
+$pass = "3087045478"; 
 $db   = "db_xirpl1-18_1";
 
+// perintah koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
+// cek status koneksi
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
